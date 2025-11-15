c4a44de51a3e32c103f6ae401c7347f6a6178577
Andre-Lucho <andretlucho@gmail.com>
2025-11-15 18:38:48 -0300

    [CHORE] Implementaçao Ambiente Dev - Vite + SASS

---
+++ portifolio-v1.2.0/script.js
@@ -0,0 +1,11 @@
+import menuTabs from './src/modules/menuTabs.js';
+import light_DarkToggleMode from './src/modules/lightToggleMode.js';
+import initsmoothScroll from './src/modules/smoothScroll.js';
+import initContactMenu from './src/modules/initContactMenu.js';
+import iniMobileMenu from './src/modules/initMobileMenu.js';
+
+menuTabs();
+light_DarkToggleMode();
+initsmoothScroll();
+initContactMenu();
+iniMobileMenu();
