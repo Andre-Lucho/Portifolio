899210d313aa4680c27a54050ed240321cbba5d9
Andre-Lucho <andretlucho@gmail.com>
2025-11-15 19:10:02 -0300

    [CHORE]Add pacote Vercel - métricas

--- portifolio-v1.2.0/script.js
+++ portifolio-v1.2.0/script.js
@@ -1,8 +1,12 @@
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 import menuTabs from './src/modules/menuTabs.js';
 import light_DarkToggleMode from './src/modules/lightToggleMode.js';
 import initsmoothScroll from './src/modules/smoothScroll.js';
 import initContactMenu from './src/modules/initContactMenu.js';
 import iniMobileMenu from './src/modules/initMobileMenu.js';
+
+injectSpeedInsights();
 
 menuTabs();
 light_DarkToggleMode();
